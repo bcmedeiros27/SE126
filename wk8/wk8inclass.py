facad3f300d9a62d0326d033b43e05d50f59aa2d
bcmedeiros27 <bcmedeiros@email.neit.edu>
2024-02-27 13:39:00 -0500

    wk8

---
+++ wk8/wk8inclass.py
@@ -0,0 +1,21 @@
+#var list
+row = [1,2,3,4,5,6,7]
+seata = ["A","A","A","A","A","A","A"]
+seatb = ["B","B","B","B","B","B","B"]
+seatc = ["C","C","C","C","C","C","C"]
+seatd = ["D","D","D","D","D","D","D"]
+#def list --------
+def term():
+    for i in range(0, len(row)):
+        print(f"{row[i]}\t\t{seata[i]} {seatb[i]}\t\t{seatc[i]} {seatd[i]}")
+allFlight = []
+for i in range(0, len(row)):
+    allFlight.append([row[i], seata[i], seatb[i], seatc[i], seatd[i]])
+print(allFlight)
+useR = int(input("enter:"))-1
+useS = int(input("enter"))
+for i in range(0, len(allFlight)):
+    for x in range(0, len(allFlight[i])):
+        print(f"{allFlight[i][x]:} ", end="") #Final index print statment
+        
+print(allFlight[6])
